037c3e7ad17ed3d4907fd7565e797b27e8548796
Artem Petrov <artempetrovjob@mail.ru>
2026-02-06 21:49:14 +0300

    feat: add routing with login, users and 404 page

---
+++ src/app/router.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "../pages/login/LoginPage";
+import { UsersPage } from "../pages/users/UsersPage";
+import { NotFoundPage } from "../pages/not-found/NotFoundPage";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
---
+++ src/pages/not-found/NotFoundPage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Result } from "antd";
+
+export const NotFoundPage = () => {
+  return <Result status="404" title="404" subTitle="Page not found" />;
+};
